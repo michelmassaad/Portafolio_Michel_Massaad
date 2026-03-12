415691b9c6425de622c42c204c89cd8069e2a125
Michel Massaad <michel_01ft@hotmail.com>
2026-03-12 11:42:54 -0300

    chore: configuración de Vite y script de deploy para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'react'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portafolio_Michel_Massaad/', // ¡Asegúrate de que tenga las barras al inicio y al final!
+  base: '/Portafolio_Michel_Massaad/',
 })
